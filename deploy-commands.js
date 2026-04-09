6c38c6f9d65a8ceac8f6fe815169cb251c6402f7
khoaanhnguyen728-wq <khoaanhnguyen728@gmail.com>
2026-04-09 13:03:48 +0700

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,15 +10,16 @@ new SlashCommandBuilder()
 o.setName("user").setDescription("User").setRequired(true)
 )
 .addIntegerOption(o =>
-o.setName("strike")
-.setDescription("Chọn strike cần gỡ")
-.setRequired(true)
-.addChoices(
-{ name: "Strike 1", value: 1 },
-{ name: "Strike 2", value: 2 },
-{ name: "Strike 3", value: 3 },
-{ name: "Strike 4", value: 4 }    
-)),
+  o.setName("strike")
+    .setDescription("Chọn strike cần gỡ")
+    .setRequired(true)
+    .addChoices(
+      { name: "Strike 1", value: 1 },
+      { name: "Strike 2", value: 2 },
+      { name: "Strike 3", value: 3 },
+      { name: "Strike 4", value: 4 }
+    )
+),
 
 new SlashCommandBuilder()
 .setName("strike")
